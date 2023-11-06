7052ca4a19b749db9e01109b8d68c9b3792ce268
Rory O Donoghue <irelander99@mail.com>
2023-11-06 12:28:44 +0000

    Commit 7 : Creating Two Intents in 'Options' : 'Home' and 'Play'

--- app/src/main/java/com/Options.kt
+++ app/src/main/java/com/Options.kt
@@ -1,28 +1,70 @@
 package com
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.oct24provisional.HomeScreen
+import com.example.oct24provisional.MainActivity
 import com.ui.theme.OCT24ProvisionalTheme
 
 class Options : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OCT24ProvisionalTheme {
+            com.example.oct24provisional.ui.theme.OCT24ProvisionalTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting3("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        // Greeting("Android")
+                        //   HomeScreen()
+                        Spacer(modifier = Modifier.height(16.dp))
+                        // Button to navigate to Options
+                        Button(
+                            onClick = {
+                                // Create an Intent to navigate to Options
+                                val intent = Intent(this@Options, MainActivity::class.java)
+                                startActivity(intent)
+                            }
+                        ) {
+                            Text("Go to Home")
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                        // Button to navigate to Options
+                        Button(
+                            onClick = {
+                                // Create an Intent to navigate to Options
+                                val intent = Intent(this@Options, Play::class.java)
+                                startActivity(intent)
+                            }
+                        ) {
+                            Text("Go to Play")
+                        }
+                        HomeScreen()
+
+                    }
+
                 }
             }
         }
