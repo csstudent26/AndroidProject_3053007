1c2c2f4f8f471340ac3aeee14c522d331d28d596
Rory O Donoghue <irelander99@mail.com>
2023-11-07 04:21:50 +0000

    Commit 12 : Adding Text to 'Options'

--- app/src/main/java/com/Options.kt
+++ app/src/main/java/com/Options.kt
@@ -3,14 +3,25 @@ package com
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ui.theme.OCT24ProvisionalTheme
+
+
+
 
 class Options : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +33,74 @@ class Options : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting4("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Top,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        // Title
+                        Text(
+                            text = "Game Options",
+                            style = TextStyle(
+                                fontSize = 20.sp, // Adjust the font size as needed
+                                fontWeight = FontWeight.Bold
+                            ),
+                            modifier = Modifier.padding(16.dp)
+                        )
+
+                        // Instructions
+                        Text(
+                            text = "Game Rules",
+                            style = TextStyle(
+                                fontSize = 16.sp, // Adjust the font size as needed
+                                fontWeight = FontWeight.Bold
+                            ),
+                            modifier = Modifier.padding(16.dp)
+                        )
+
+                        Text(
+                            text = "In this dice game, you'll compete against the house. Here are the rules:",
+                            style = TextStyle(fontSize = 14.sp),
+                            modifier = Modifier.padding(8.dp)
+                        )
+
+                        Text(
+                            text = "1. Each player receives a throw consisting of two dice.",
+                            style = TextStyle(fontSize = 14.sp),
+                            modifier = Modifier.padding(8.dp)
+                        )
+
+                        Text(
+                            text = "2. The sum of each throw determines the winner of that round.",
+                            style = TextStyle(fontSize = 14.sp),
+                            modifier = Modifier.padding(8.dp)
+                        )
+
+                        Text(
+                            text = "3. If you roll a 'six,' you have the option to re-roll the lower die.",
+                            style = TextStyle(fontSize = 14.sp),
+                            modifier = Modifier.padding(8.dp)
+                        )
+
+                        Text(
+                            text = "4. The house can take a second throw once in a game.",
+                            style = TextStyle(fontSize = 14.sp),
+                            modifier = Modifier.padding(8.dp)
+                        )
+
+                        // Game Settings
+                        Text(
+                            text = "Game Settings",
+                            style = TextStyle(
+                                fontSize = 16.sp, // Adjust the font size as needed
+                                fontWeight = FontWeight.Bold
+                            ),
+                            modifier = Modifier.padding(16.dp)
+                        )
+
+                        // Include options for users to configure game settings, such as sound, visuals, difficulty, and theme.
+                        // You can use buttons, switches, or other Composable elements to allow users to customize their gaming experience.
+                    }
                 }
             }
         }
