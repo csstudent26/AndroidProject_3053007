bf77dbf2c5b3a95dd9a3c3d67db181dbd01f4552
Rory O Donoghue <irelander99@mail.com>
2023-11-10 07:03:29 +0000

    Commit 24 : Altering Composable 'PlayScreen' in 'Play'

--- app/src/main/java/com/Play.kt
+++ app/src/main/java/com/Play.kt
@@ -1,5 +1,6 @@
 package com
 
+import androidx.navigation.NavController
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,34 +9,70 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.tooling.preview.Preview
 import com.ui.theme.OCT24ProvisionalTheme
 import androidx.compose.foundation.layout.fillMaxSize
 
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
+
+
+
+import androidx.compose.runtime.remember
+
+
+
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+
+
+
+
+
 import androidx.compose.ui.viewinterop.AndroidView
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.TextStyle
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+import kotlinx.coroutines.delay
 class Play : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Create a NavController
+
+
         setContent {
             OCT24ProvisionalTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ){
+                ) {
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
@@ -44,7 +81,7 @@ class Play : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Text(
-                            text = "Ready To Play",
+                            text = "Ready To Play ?",
                             style = TextStyle(
                                 fontSize = 24.sp,
                                 fontWeight = FontWeight.Bold
@@ -57,37 +94,97 @@ class Play : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview3() {
-    OCT24ProvisionalTheme {
-        Greeting3("Android")
+    @Composable
+    fun Greeting3(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
     }
-}
-@Composable
-fun PlayScreen() {
-    val gifUrl = "https://media.tenor.com/5En-9o6PM6UAAAAC/dice.gif" // Replace with your GIF URL
 
-    AndroidView(
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview3() {
+        OCT24ProvisionalTheme {
+            Greeting3("Android")
+        }
+    }
 
-        modifier = Modifier.fillMaxSize(),
-        factory = { context ->
-            WebView(context).apply {
-                loadUrl(gifUrl)
+    @Composable
+    fun PlayScreen() {
+        val gifUrl =
+            "https://media.tenor.com/5En-9o6PM6UAAAAC/dice.gif" // Replace with your GIF URL
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            AndroidView(
 
-                settings.javaScriptEnabled = true
-                webViewClient = WebViewClient()
+                modifier = Modifier.fillMaxSize(),
+                factory = { context ->
+                    WebView(context).apply {
+                        loadUrl(gifUrl)
+
+                        settings.javaScriptEnabled = true
+                        webViewClient = WebViewClient()
+                    }
+                }
+            )
+            // Button to start the game and navigate to the DieResultScreen
+            Button(
+                onClick = {
+                    // Start the game and navigate to the DieResultScreen
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+            ) {
+                Text(text = "Start Game")
             }
         }
-    )
+
+
+        @Composable
+        fun DieResultScreen() {
+            // Simulate the result of a die throw
+            val die1 = (1..6).random()
+            val die2 = (1..6).random()
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Gray)
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Die 1: $die1",
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        ),
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Text(
+                        text = "Die 2: $die2",
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        ),
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
 }
